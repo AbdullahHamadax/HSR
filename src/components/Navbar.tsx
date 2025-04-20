46e224c91c410b6261450222fc7cf70da500336a
AbdullahHamadax <abdallahamada114@gmail.com>
2025-04-20 18:17:56 +0200

    Change logo format to .webp and change the image in about page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import { AnimatePresence, motion } from "framer-motion";
 import { Menu, X } from "lucide-react";
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
-import logo from "../images/logo.png";
+import logo from "../images/logo.webp";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
